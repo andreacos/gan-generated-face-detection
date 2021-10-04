dd4bced495ca614f1c42d2997179be8f4247d723
andreacos <andreacos82@gmail.com>
2021-10-04 15:19:29 +0200

    Update Train_xception.py

--- Train_xception.py
+++ Train_xception.py
@@ -5,23 +5,13 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import configuration as config
 from glob import glob
-from network import xception_net, efficient_net
+from network import xception_net
 from tensorflow.keras.utils import plot_model
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from custom_generators import SimpleBatchGenerator
 from utils import plot_training_history
 from tensorflow.keras.models import load_model
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    except RuntimeError as e:
-        # Memory growth must be set before GPUs have been initialized
-        print(e)
 
 if __name__ == '__main__':
 
@@ -75,12 +65,7 @@ if __name__ == '__main__':
     def initialize_network(fine_tune_model_file='', network_type='xception'):
 
         if fine_tune_model_file == '':
-
-            if network_type == 'xception':
-                model = xception_net(in_shape=(config.XCP_INPUT_SIZE, config.XCP_INPUT_SIZE, config.XCP_INPUT_CHANNELS))
-            else:
-                model = efficient_net(in_shape=(config.XCP_INPUT_SIZE, config.XCP_INPUT_SIZE, config.XCP_INPUT_CHANNELS))
-
+            model = xception_net(in_shape=(config.XCP_INPUT_SIZE, config.XCP_INPUT_SIZE, config.XCP_INPUT_CHANNELS))
         else:
             print(f"Fine-tuning from model {fine_tune_model_file}")
             model = load_model(fine_tune_model_file)
@@ -89,23 +74,12 @@ if __name__ == '__main__':
                                                                  'ckpt.epoch{epoch:02d}-loss{val_loss:.2f}.h5'),
                                            monitor='val_loss', verbose=1, save_weights_only=False, save_best_only=False)
 
-        if network_type == 'xception':
+        lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), cooldown=0, patience=2, min_lr=0.5e-6)
+        callbacks = [lr_reducer, checkpoint_saver]
 
-            lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), cooldown=0, patience=2, min_lr=0.5e-6)
-            callbacks = [lr_reducer, checkpoint_saver]
-
-            model.compile(loss=tf.keras.losses.categorical_crossentropy,
-                          optimizer=tf.keras.optimizers.Adam(learning_rate=1e-2),
-                          metrics=['accuracy'])
-
-        else:
-
-            learning_rate_fn = tf.keras.optimizers.schedules.PolynomialDecay(1e-2, 80000, 1e-5, power=0.8)
-            callbacks = [checkpoint_saver]
-
-            model.compile(loss=tf.keras.losses.categorical_crossentropy,
-                          optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate_fn),
-                          metrics=['accuracy'])
+        model.compile(loss=tf.keras.losses.categorical_crossentropy,
+                      optimizer=tf.keras.optimizers.Adam(learning_rate=1e-2),
+                      metrics=['accuracy'])
 
         # Display a summary of configuration and network
         print(model.summary())
@@ -113,7 +87,7 @@ if __name__ == '__main__':
 
         return model, callbacks
 
-    exp_id = f"Xception-augmentation-ep-{config.XCP_NUM_EPOCHS}-face-dataset-with-nvidia-and-print-scan"
+    exp_id = f"Xception-augmentation-ep-{config.XCP_NUM_EPOCHS}-face-dataset"
     print(f"Experiment id: {exp_id}")
 
     print(f"Creating training and validation data sets ... ")
@@ -139,11 +113,11 @@ if __name__ == '__main__':
                                  epochs=config.XCP_NUM_EPOCHS)
 
     # Save history
-    # print(f"Saving training history ... ")
-    # with open(f'{exp_id}training-history.pkl', 'wb') as file_pi:
-    #     pickle.dump(history.history, file_pi)
+    print(f"Saving training history ... ")
+    with open(f'{exp_id}training-history.pkl', 'wb') as file_pi:
+        pickle.dump(history.history, file_pi)
 
     # Plot training history (accuracy and validation)
-    # print("Plotting training history ... ")
-    # history = pickle.load(open(f'{exp_id}training-history.pkl'))
-    # plot_training_history(history, f"{exp_id}")
+    print("Plotting training history ... ")
+    history = pickle.load(open(f'{exp_id}training-history.pkl'))
+    plot_training_history(history, f"{exp_id}")
